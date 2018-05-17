06a71fd6e210c83f6d49d74fa01faf69eebb6411
root <root@diffusionkinetics.com>
2018-05-17 11:22:34 +0000

    form repeater

---
+++ public/dkjs/form-repeat.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".input_fields_wrap"); //Fields wrapper
+    var add_button = $(".add_field_button"); //Add button ID
+    
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function (e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            var cloned_elem = $(".cloneable:last").clone();
+            cloned_elem.children('input').val("");
+            cloned_elem.removeClass("first_row")
+            //cloned_elem.append('<button class="remove_field">-</button>');
+            $(wrapper).append(cloned_elem); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
